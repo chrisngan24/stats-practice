2f2d07fd40fc2f0a584e184b1433e15f1dc93fff
chrisngan24 <chris.ngan24@gmail.com>
2015-01-22 02:41:37 +0000

    finished assignment

--- course_work/syde_334/tutorial_1/tut_1.R
+++ course_work/syde_334/tutorial_1/tut_1.R
@@ -37,6 +37,7 @@ q2 <- function(){
   b0 <- 1
   b1 <- 2
   m_sd <- 3.5
+  sxx <- sum((x-xbar)^2)
   
   y <- simp_lm_gen(x, b0, b1, m_sd)
   # part 1 - calculate MLE for b0 and b1
@@ -52,7 +53,8 @@ q2 <- function(){
   abline(b0_mle, b1_mle)
   
   # part 3 - calculate 92% conf interval for b1_mle
-  error <- qnorm(0.92)*m_sd
+  alpha <- 0.92
+  error <- qnorm(1-(1-alpha)/2)*m_sd/sqrt(sxx)
   left <- b1_mle - error
   right <- b1_mle + error
   print(c('92% confidence interval: [', left, ',', right,']'))
@@ -63,6 +65,9 @@ q3 <- function(){
   N <- 1000
   M <- 10000
   x <- rnorm(N, 0, 5)
+  xbar <- mean(x)
+  sxx <- sum((x-xbar)^2)
+
   b0 <- 1
   b1 <- 2
   m_sd <- 3.5
@@ -71,13 +76,38 @@ q3 <- function(){
   
   # Part 1 - calculate b1_mle, plot histogram
   b1_mle_mat <- apply(y_mat, 2, mle_b1, x)
-  hist(b1_mle_mat)
+  hist(b1_mle_mat, freq = FALSE,  breaks = 100, xlab = "beta 1 hat") 
+  curve(dnorm(x, mean = b1, sd = m_sd/sqrt(sxx)), add = TRUE, col = "red")
   
-  return(c(y_mat, b1_mle_mat))
+  # Part 2 - calculate 92% confidence interval 
+  # how many estimates of b1 are covered in interval?
+  alpha <- 0.92
+  error <- qnorm(1-(1-0.92)/2)*m_sd/sqrt(sxx)
+  lower_bound <- b1_mle_mat - error
+  upper_bound <- b1_mle_mat + error
+  contains <- b1 > lower_bound & b1 < upper_bound
+  in_conf_int <- sum(contains)/length(contains)  
+  print(in_conf_int)  
+  
+  # Part 3 - predict sd of 
+  n_sub <- 10
+  y_10 <- y_mat[c(1:n_sub),]
+  x_10 <- x[c(1:n_sub)]
+  var_hat <- rep(NA, M)
+  for (ii in 1:M){
+    b1_mle <- mle_b1(y_mat[,ii],x)
+    b0_mle <- mle_b0(y_mat[,ii],x,b1_mle)
+  
+    var_hat[ii] <- sum((y_10[,ii] - b0_mle - b1_mle*x_10)^2) / (n_sub - 2)
+  }
+  
+  print(c('Estimated sd:', sqrt(mean(var_hat)), ' Given sd:', m_sd))
+  
+#  return(c(y_mat, b1_mle_mat))
   
   
 }
 
-
-q2()
-list[y_mat, b1_mle_mat] <- q3()
+#q2()
+#list[y_mat, b1_mle_mat] <- q3()
+contains <- q3()
